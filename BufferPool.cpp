d989338041440c97b2243b85d74adc9971579c18
2023JoshuaP <jphilco@unsa.edu.pe>
2024-05-30 11:21:31 -0500

    Implementacion de las funciones de BufferPool.

--- BufferPool.cpp
+++ BufferPool.cpp
@@ -1,1 +1,27 @@
 #include "BufferPool.h"
+
+BufferPool::BufferPool() {}
+
+BufferPool::~BufferPool() {}
+
+void BufferPool::agregarNuevaPaginaBufferPool(int numFrame, int numPagina) {
+    if (this->vectorFramesBufferPool[numFrame].paginaEnFrame.vectorRegistrosEnPagina.empty()) {
+        this->vectorFramesBufferPool[numFrame].leerDatosEnPaginaDeFrame(numPagina);
+    }
+    else {
+        this->vectorFramesBufferPool[numFrame].paginaEnFrame.vectorRegistrosEnPagina.clear();
+        this->vectorFramesBufferPool[numFrame].leerDatosEnPaginaDeFrame(numPagina);
+    }
+}
+
+void BufferPool::mostrarFramePagina(int numFrame) {
+    this->vectorFramesBufferPool[numFrame].paginaEnFrame.mostrarContenidoPagina();
+}
+
+void BufferPool::lecturaOescrituraPagina(int numFrame) {
+    this->vectorFramesBufferPool[numFrame].paginaEnFrame.leerOescribirPAgina();
+}
+
+void BufferPool::agregarContenidoPaginaABloque(int numFrame, int numPaginaEliminada) {
+    this->vectorFramesBufferPool[numFrame].paginaEnFrame.agregarContenido(numPaginaEliminada);
+}
