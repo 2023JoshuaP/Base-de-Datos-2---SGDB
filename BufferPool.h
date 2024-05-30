bfc1f7a244b6b179ae96ceee387cd6a21be92d81
2023JoshuaP <jphilco@unsa.edu.pe>
2024-05-30 11:12:24 -0500

    Definiendo las funciones y atributos para el BufferPool.

--- BufferPool.h
+++ BufferPool.h
@@ -3,7 +3,19 @@
 
 #include <vector>
 #include <iostream>
+#include "./Frame.cpp"
 
-class BufferPool {};
+class BufferPool {
+    public:
+        int capacidadDeCadaFrame;
+        vector<Frame> vectorFramesBufferPool;
+    public:
+        BufferPool();
+        ~BufferPool();
+        void agregarNuevaPaginaBufferPool(int numFrame, int numPagina);
+        void mostrarFramePagina(int numPagina);
+        void lecturaOescrituraPagina(int numPagina);
+        void agregarContenidoPaginaABloque(int numPagina, int numPaginaEliminada);
+};
 
 #endif
