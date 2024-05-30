77ad4c72e0c1afa81b0ab130e55a363c210ef899
2023JoshuaP <jphilco@unsa.edu.pe>
2024-05-30 12:01:26 -0500

    Definiendo atributos y clases de BufferManager.

--- BufferManager.h
+++ BufferManager.h
@@ -4,7 +4,22 @@
 #include <vector>
 #include <sstream>
 #include <string>
+#include "./PageTable.cpp"
+#include "./BufferPool.cpp"
 
-class BufferManager {};
+class BufferManager {
+    public:
+        BufferPool bufferPool;
+        PageTable pageTable;
+    public:
+        BufferManager();
+        ~BufferManager();
+        void crearBufferPoolSegunFrames(int numFrames);
+        void establecerLimitedeFrames(int pesoBytesBloques);
+        void obtenerUnaPagina(int numPagina);
+        void mostrarUnaPagina(int numPagina);
+        void mostrarPageTable();
+        void dejarDeUsarPagina(int numPagina);
+};
 
 #endif
